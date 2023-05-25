2e93cb8f958591739f1ba228387c0e97eb141e42
WyattChase <esnaruto@yahoo.com>
2023-05-24 21:33:46 -0400

    added changes to title and added scoring needs fixing though

--- Pong Disaster/main.cpp
+++ Pong Disaster/main.cpp
@@ -1,11 +1,16 @@
 #include "raylib.h"
+#include <string>
 
-const int screenWidth = 800;
-const int screenHeight = 450;
+const int screenWidth = 900;
+const int screenHeight = 500;
 
 const int paddleWidth = 20;
 const int paddleHeight = 80;
 const int paddleSpeed = 5;
+
+int scorePlayer1 = 0;
+int scorePlayer2 = 0;
+
 
 
 
@@ -20,6 +25,7 @@ int main()
 	Vector2 ballSpeed = { 3, 3 };
 
 	SetTargetFPS(60);
+	
 
 	while (!WindowShouldClose())
 	{
@@ -34,6 +40,13 @@ int main()
 		if (IsKeyDown(KEY_DOWN) && player2.y + player2.height < screenHeight)
 			player2.y += paddleSpeed;
 
+		if (IsKeyPressed(KEY_ENTER))
+		{
+			// Restart the game
+			scorePlayer1 = 0;
+			scorePlayer2 = 0;
+		}
+
 
 		ballPosition.x += ballSpeed.x;
 		ballPosition.y += ballSpeed.y;
@@ -46,6 +59,27 @@ int main()
 		if (CheckCollisionCircleRec(ballPosition, 10, player1) || CheckCollisionCircleRec(ballPosition, 10, player2))
 			ballSpeed.x *= -1;
 
+		if (ballPosition.x > screenWidth)
+		{
+			// Player 1 scores a point
+			scorePlayer1++;
+
+			// Resets the ball position and speed for the next round
+			ballPosition = { screenWidth / 2, screenHeight / 2 };
+			ballSpeed = { -3, 3 };
+
+		}
+		else if (ballPosition.x < 0)
+		{
+			// Player 2 scores a point
+			scorePlayer2++;
+
+			// Resets the ball position and speed for the next round
+			ballPosition = { screenWidth / 2, screenHeight / 2 };
+			ballSpeed = { -3, 3 };
+
+		}
+
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
@@ -54,10 +88,25 @@ int main()
 			DrawRectangleRec(player2, WHITE);
 			DrawCircleV(ballPosition, 10, WHITE);
 
-			DrawText("Player 1 Moves: W = Up and S = Down", 10, 10, 20, WHITE);
-			DrawText("Player 1 Moves: Arrow Up = Up and Arrow Down = Down", 170, 10, 20, WHITE);
+			DrawText("Player 1: W = Up and S = Down", 10, 10, 20, WHITE);
+
+			DrawText("Player 2: Arrow Up = Up and Arrow Down = Down", screenWidth - 500, 10, 20, WHITE);
+
+			DrawText(("Player 1:" + std::to_string(scorePlayer1)).c_str(), 10, 10, 20, WHITE);
+			DrawText(("Player 2:" + std::to_string(scorePlayer2)).c_str(), screenWidth - 500, 10, 20, WHITE);
+
+
+
 
 		}
+		const int winningScore = 5;
+		if (scorePlayer1 >= winningScore || scorePlayer2 >= winningScore)
+		{
+			//Game over condition met, display a "Game Over" message
+			DrawText("Game Over", screenWidth / 2 - MeasureText("Game Over", 40) / 2, screenHeight / 2 - 40, 40, WHITE);
+		}
+
+
 		EndDrawing();
 	}
 
